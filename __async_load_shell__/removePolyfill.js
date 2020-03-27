d7730de9475f138fc361b7e19794cddf3f48b9bf
Ji Ma <ji.ma@ndkey.com.cn>
2020-03-27 15:29:47 +0800

    optime

--- __async_load_shell__/removePolyfill.js
+++ __async_load_shell__/removePolyfill.js
@@ -1,5 +1,5 @@
-const fs = require('fs');
-const readline = require('readline');
+const fs = require("fs");
+const readline = require("readline");
 // Functions
 
 // Main
@@ -8,15 +8,15 @@ let filePath = argvs[0];
 
 var fRead = fs.createReadStream(filePath);
 var objReadline = readline.createInterface({
-  input: fRead,
+  input: fRead
 });
 let diff = new Array();
-objReadline.on('line', function(line) {
-  if (line.startsWith('__d') || line.startsWith('__r')) {
+objReadline.on("line", function(line) {
+  if (line.startsWith("__d") || line.startsWith("__r")) {
     diff.push(line);
   }
 });
-objReadline.on('close', function() {
-  let data = diff.join('\n');
-  fs.writeFileSync(filePath, data); // 生成 diff.ios.jsbundle
+objReadline.on("close", function() {
+  let data = diff.join("\n");
+  fs.writeFileSync(filePath, data);
 });
